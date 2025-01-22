afcca6b019419bca105294e0229aca29842cf37c
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-22 11:06:25 +0900

    exercise1

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,8 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food
+    @foods = Food.left_outer_joins(:order_foods).where(order_foods: {order_id: nil})
+    
   end
 
   def exercise2
